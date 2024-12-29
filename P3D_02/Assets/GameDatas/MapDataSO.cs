f1c42159efe221e8ea9f7fdb38c6c1241221f559
TuanBaoNguyen <nguyentuanbao032000@gmail.com>
2024-12-29 22:48:25 +0700

    Working on MapData

---
+++ P3D_02/Assets/GameDatas/MapDataSO.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MapDataSO", menuName = "MapData/New MapDataSO")]
+public class MapDataSO : ScriptableObject
+{
+    public List<MapData> MapDatas = new List<MapData>();
+}
+
+[Serializable]
+public class MapData
+{
+    public string trackName;
+    public string description;
+    public Sprite thumbnail;
+
+    public List<Vector3> startPositions;
+    public List<Vector3> waypoints;
+}
--- P3D_02/Assets/Scripts/Cars/WaypointsManager.cs
+++ P3D_02/Assets/Scripts/Cars/WaypointsManager.cs
@@ -4,35 +4,38 @@ using UnityEngine;
 public class WaypointsManager : MonoBehaviour
 {
     public List<Transform> waypoints = new List<Transform>();
+    public List<Transform> startpoints = new List<Transform>();
     public bool isShowGizmos;
 
     private void OnDrawGizmos()
     {
         DrawWaypoints();
     }
-
-    /// <summary>
-    /// Vẽ các waypoint và nối chúng bằng Gizmos.
-    /// </summary>
     private void DrawWaypoints()
     {
         if (!isShowGizmos) return;
 
+        //Draw waypoints 
         Gizmos.color = Color.green;
-
         for (int i = 0; i < waypoints.Count; i++)
         {
-            // Vẽ vị trí waypoint
             if (waypoints[i] != null)
             {
                 Gizmos.DrawSphere(waypoints[i].position, 0.3f);
 
-                // Nối với waypoint tiếp theo
                 if (i < waypoints.Count - 1 && waypoints[i + 1] != null)
                 {
                     Gizmos.DrawLine(waypoints[i].position, waypoints[i + 1].position);
                 }
             }
+        }
+
+        //Draw startpoints
+        Gizmos.color = Color.blue;
+        for (int i = 0; i < startpoints.Count; i++)
+        {
+            if (startpoints[i] != null)
+                Gizmos.DrawSphere(startpoints[i].position, 0.3f);
         }
     }
 
