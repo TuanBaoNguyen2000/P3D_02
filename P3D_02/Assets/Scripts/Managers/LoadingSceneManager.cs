342c08595013dc0f0a1b748894487daa29aac837
TuanBaoNguyen <nguyentuanbao032000@gmail.com>
2024-12-29 17:11:38 +0700

    Add Loading Scene

---
+++ P3D_02/Assets/Scripts/Managers/LoadingSceneManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingSceneManager : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] private Slider progressBar;
+    [SerializeField] private Text progressText;
+    [SerializeField] private Text tipsText;
+
+    [SerializeField] private string[] tips;
+
+
+    private void Start()
+    {
+        if (tips != null && tips.Length > 0)
+        {
+            tipsText.text = tips[Random.Range(0, tips.Length)];
+        }
+
+        StartCoroutine(LoadSceneAsync()); 
+    }
+
+    private IEnumerator LoadSceneAsync()
+    {
+        string sceneName = SceneLoader.TargetScene;
+        if (string.IsNullOrEmpty(sceneName)) sceneName = "MainHomeScene";
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            progressBar.value = progress;
+            progressText.text = (progress * 100).ToString("F0") + "%";
+
+            if (operation.progress >= 0.9f)
+            {
+                yield return new WaitForSeconds(1f);
+                operation.allowSceneActivation = true; 
+            }
+
+            yield return null;
+        }
+    }
+}
